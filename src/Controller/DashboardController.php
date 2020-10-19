a8a2fc9c4d3c0d83b2dd15010c69808823e387e2
Amosconstantjunior <junzgamos@gmail.com>
2020-10-19 19:49:07 +0200

    deploye 12

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\FicheMaintenanceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,10 +11,11 @@ class DashboardController extends AbstractController
     /**
      * @Route("/dashboard", name="dashboard")
      */
-    public function index()
+    public function index(FicheMaintenanceRepository $ficheMaintenanceRepository)
     {
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
+            'fiche_maintenances' => $ficheMaintenanceRepository->findAll()
         ]);
     }
 }
